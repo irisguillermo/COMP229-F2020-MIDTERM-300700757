4c534156d2a12589232206aede477b42afd4e7e1
Iris G <iguiller@my.centennialcollege.ca>
2022-10-27 13:33:18 -0400

    final codes

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,3 @@
 //export const MongoURI = "mongodb://localhost:27017/media";
 export const Secret = "someSecret";
-export const MongoURI = "mongodb+srv://kshatriairisg:7Kk8p17<3@cluster0.hbvm22j.mongodb.net/?retryWrites=true&w=majority";
+export const MongoURI = "mongodb+srv://kshatriairisg:7Kk8p17<3@cluster0.hbvm22j.mongodb.net/booksmidterm?retryWrites=true&w=majority";
