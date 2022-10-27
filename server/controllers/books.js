d70fa0284c1d18966e6c505850b54bd9d6cf3b62
Iris G <iguiller@my.centennialcollege.ca>
2022-10-27 13:07:51 -0400

    added codes

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -1,4 +1,6 @@
 // define the book model
+import { render } from 'ejs';
+import books from '../models/books.js';
 import booksModel from '../models/books.js';
 
 /* GET books List page. READ */
@@ -12,30 +14,42 @@ export function displayBookList(req, res, next) {
         res.render('index', { title: 'Book List', page: 'books/list', books: booksCollection });
     });
 }
-
+//start here for video demo
 //  GET the Book Details page in order to add a new Book
 export function displayAddPage(req, res, next) {
-
-    /*****************
-    * ADD CODE HERE *
-    *****************/
+    res.render('index', {title: 'Add Books', page: 'books/add', books: {}});
 }
 
 // POST process the Book Details page and create a new Book - CREATE
 export function processAddPage(req, res, next) {
+    let newBook = booksModel({
+        name: req.body.name,
+        author: req.body.author,
+        published: req.body.published,
+        description: req.body.description,
+        price: req.body.price
+    });
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    booksModel.create(newBook, (err, Books) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        };
+        res.redirect('/books/list')
+    })
 }
 
 // GET the Book Details page in order to edit an existing Book
 export function displayEditPage(req, res, next) {
+    let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
-
+    booksModel.findById(id, (err, books) =>{
+        if (err){
+            console.error(err);
+            red.end(err);
+        }
+        res.render('index', {title: 'Edit Books', page: 'books/edit', books: books})
+    })
 }
 
 // POST - process the information passed from the details form and update the document
@@ -47,7 +61,13 @@ export function processEditPage(req, res, next) {
 
 // GET - process the delete by user id
 export function processDelete(req, res, next) {
-    /*****************
-  * ADD CODE HERE *
-  *****************/
+    let id = req.params.id;
+
+    booksModel.remove({_id: id}, (err) => {
+        if (err){
+            console.error(err);
+            res.end(err);
+        }
+        res.redirect('/books/list');
+    })
 }
